79423f9779efc4971df2c0e5d37aa4e3f5df0ed5
Charlie Clark <charlie.clark@clark-consulting.eu>
2015-05-13 16:39:11 +0200

    Backout changeset f9e5e2b2a6ed9324bc989aeba12f6919b55b664e

--- openpyxl/benchmarks/styles.py
+++ openpyxl/benchmarks/styles.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 from itertools import product
 from random import Random
 from tempfile import TemporaryFile
@@ -62,7 +61,7 @@ def to_profile(wb, f, n):
     print('took %.4fs for %d styles' % (t + time.time(), n))
 
 for func in (optimized_workbook, non_optimized_workbook):
-    print('%s: ' % func.__name__,)
+    print '%s: ' % func.__name__,
     wb = func(styles)
     f = TemporaryFile()
     to_profile(wb, f, n)
